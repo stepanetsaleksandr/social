3e824ca47a1517cdd7de87e6267a076eeed52c56
Aleksandr <stepanetsaleksandr@gmail.com>
2021-07-06 12:53:56 +0300

    stage12

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./Header";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-    <header className='header'>
-      <img src='./logo192.png'/>
-    </header>
-      <nav className='nav'>
-        <div><a>Profile</a></div>
-        <div><a>Messages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Settings</a></div>
-      </nav>
-      <div className='content'>
-        <div> <img src='https://landlord.ua/wp-content/uploads/2020/07/CL-20.026.001-1-920x418.jpg'/></div>
-        <div>ava+description</div>
-        <div>
-          My posts
-          <div>
-            <div>Post 1 </div>
-            <div>Post 2 </div>
-          </div>
-        </div>
-
-      </div>
+        <Header/>
+        <Navbar/>
+        <Profile/>
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div> <img src='https://landlord.ua/wp-content/uploads/2020/07/CL-20.026.001-1-920x418.jpg'/></div>
+            <div>ava+description</div>
+            <div>
+                My posts
+                <div>
+                    <div>Post 1 </div>
+                    <div>Post 2 </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default Profile;
