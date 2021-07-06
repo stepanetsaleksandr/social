39311bd68cb159bc9223a6e498324ac99606f342
Aleksandr <stepanetsaleksandr@gmail.com>
2021-07-06 12:47:36 +0300

    1st

--- src/App.js
+++ src/App.js
@@ -9,12 +9,23 @@ const App = () => {
       <img src='./logo192.png'/>
     </header>
       <nav className='nav'>
-        <div>Profile</div>
-        <div>Messages</div>
-        <div>Music</div>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
       </nav>
       <div className='content'>
-        Main content
+        <div> <img src='https://landlord.ua/wp-content/uploads/2020/07/CL-20.026.001-1-920x418.jpg'/></div>
+        <div>ava+description</div>
+        <div>
+          My posts
+          <div>
+            <div>Post 1 </div>
+            <div>Post 2 </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
